6e9e662459cd4d6ae766ecb2f23c448a92203663
Mohit Bhatia <mohitbhatia1994@gmail.com>
2015-11-29 22:57:35 -0500

    commit

--- web/js/bills.js
+++ web/js/bills.js
@@ -26,7 +26,9 @@ function addFunc(event){
         allBills.set("Note", note);
         allBills.set("Amount", amount);
         allBills.set("Resolved", false);
-        allBills.set("PaidBy" , Parse.User.current().objectId);
+        allBills.set("PaidBy" , Parse.User.currentUser.getObjectId());
+        allBills.set("PaidBy12" , currentUser.getObjectId());
+
         allBills.save(null, {
             sucess: function(allBills){
                 window.location.replace("bills.html");
