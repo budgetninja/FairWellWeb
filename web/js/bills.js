11364c23dce51830e0e4c8b8e6bbed97068f03d4
Mohit Bhatia <mohitbhatia1994@gmail.com>
2015-11-29 23:38:20 -0500

    commit

--- web/js/bills.js
+++ web/js/bills.js
@@ -23,12 +23,12 @@ function addFunc(event){
         var AllBills = Parse.Object.extend("AllBills");
         var allBills = new AllBills();
         var CurrentUser = Parse.User.current();
-        var CurrentUserId = CurrentUser.id;
+        var CurrentUserId = String(CurrentUser.id);
         allBills.set("UserID", uID);
         allBills.set("Note", note);
         allBills.set("Amount", amount);
         allBills.set("Resolved", false);
-        allBills.set("PaidBy",CurrentUserId );
+        allBills.set("Pby",CurrentUserId );
 
         allBills.save(null, {
             sucess: function(allBills){
